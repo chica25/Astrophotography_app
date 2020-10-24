00e0a3b911ad3d8681b7b5644d71bbc2db6768b9
chica25 <patricia@patriciaco.com>
2020-10-24 07:58:13 -0400

    Modify Nested Routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,18 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#omniauth'
 
-
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+    
   resources :users
+  #resources :stargazings, only: [:index]
+  
+  resources :stargazings
+  
+    resources :constellations do
+      resources :stargazings
+        #resources :stargazings, except: [:index]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
