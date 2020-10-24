c862153b146eab6bd3d9278cd121302860468763
chica25 <patricia@patriciaco.com>
2020-10-24 05:19:44 -0400

    Create Session, user loging, logout links and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,5 +8,10 @@ class SessionsController < ApplicationController
 
     def home
     end
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,24 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    def signup    
+    #signup 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+        #  flash.now[:message] = "You're in!"
+          redirect_to users_path(@user)
+        else
+            # flash.now[:message] = "Please try again"
+            render :new
+        end
+    end
+
+        def index
+        end
+        
+    private
+    def user_params
+        params.require(:user).permit(:username, :password, :profile_image, :bio)
     end
 end
