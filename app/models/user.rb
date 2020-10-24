c446adf76f8deb672c8721fa81f4f55014aeb7bd
chica25 <patricia@patriciaco.com>
2020-10-24 04:36:30 -0400

    Set User Model association and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+    has_many :stargazings
+    has_many :constellations, through: stargazings
     has_secure_password
+    validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
+
+
